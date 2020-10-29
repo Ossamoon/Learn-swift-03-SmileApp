efbcde20b979f1421d4806e4d03698220c5e1572
Ossamoon <osamu950623@gmail.com>
2020-10-29 21:30:48 +0900

    書籍の内容に従い完成

---
+++ SmileApp/SmileApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmileApp
+//
+//  Created by 齋藤修 on 2020/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(systemName:"smiley")
+            .resizable()
+            .foregroundColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .scaledToFit()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmileApp/SmileApp/SmileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileAppApp.swift
+//  SmileApp
+//
+//  Created by 齋藤修 on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SmileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
